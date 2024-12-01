351b808c6a517ef87b1478f46f461414d9a7091d
Adrien Loup <adloup@gmail.com>
2024-12-01 18:54:46 +0100

    feat(home): add home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.scss";
+
+function HomePage() {
+  return (
+    <h1 className={styles.title}>
+      Un trombone. Deux trombones. Trois trombones…
+    </h1>
+  );
+}
+
+export default HomePage;

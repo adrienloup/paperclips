6074db89b814bc4e2cc6b5b7c07d5cf5d7559c2f
Adrien Loup <adloup@gmail.com>
2024-12-01 18:17:32 +0100

    feat(init): add text and icons fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("paperclips")!).render(
+createRoot(document.getElementById("_paperclips_1mm2_3")!).render(
   <StrictMode>
     <App />
   </StrictMode>

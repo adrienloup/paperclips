304126bbd71d6d53fef605ffc3a51ce1dc69a574
Adrien Loup <adloup@gmail.com>
2024-12-01 19:04:16 +0100

    feat(home): add home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
 
 function HomePage() {
   return (
-    <h1 className={styles.title}>
-      Un trombone. Deux trombones. Trois trombones…
-    </h1>
+    <div>
+      <h1 className={styles.title}>
+        Un trombone. Deux trombones. Trois trombones…
+      </h1>
+      <div>
+        <Link to="/paperclips">Home</Link>
+        <br />
+        <Link to="/paperclips/help">Help</Link>
+      </div>
+    </div>
   );
 }
 

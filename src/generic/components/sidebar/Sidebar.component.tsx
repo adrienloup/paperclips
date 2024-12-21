32bef2fee802a9e1f0a59fd1e177c24339a4a0c7
Adrien Loup <adloup@gmail.com>
2024-12-22 00:39:02 +0100

    feat(35): add sidebar component

---
+++ src/generic/components/sidebar/Sidebar.component.tsx
@@ -0,0 +1,11 @@
+import { classNames } from '../../utils/classNames';
+import { Card } from './Card.type';
+import styles from './Card.module.scss';
+
+export const CardComponent = ({ children, className }: Card) => {
+  return (
+    <div className={classNames([styles.card, className])} role="complementary">
+      {children}
+    </div>
+  );
+};

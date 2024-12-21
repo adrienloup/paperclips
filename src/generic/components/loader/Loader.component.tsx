487bcd2bac7a0427a30f54d0966b236e6c57adc1
Adrien Loup <adloup@gmail.com>
2024-12-22 00:40:28 +0100

    feat(38): add loader component

--- src/generic/components/loader/Loader.component.tsx
+++ src/generic/components/loader/Loader.component.tsx
@@ -5,7 +5,7 @@ import styles from './Loader.module.scss';
 export const LoaderComponent = ({
   className,
   duration = 1e3,
-  size = 'm',
+  size = 'medium',
 }: Loader) => {
   return (
     <div

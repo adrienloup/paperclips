e7f20f097df6516981c7ce5bd5c3b6e699c6be32
Adrien Loup <adloup@gmail.com>
2024-12-21 21:38:14 +0100

    feat(17): add i18n

--- src/game/languages/en.ts
+++ src/game/languages/en.ts
@@ -3,5 +3,22 @@ import { Translation } from '../../generic/types/Translation.type';
 export const en: Translation = {
   game: {
     title: 'Paperclips',
+    manufacturing: 'Manufacturing',
+    clipsPerSecond: 'Paperclips per second: {{value}}',
+    business: 'Business',
+    fundsAvailable: 'Funds available: $\f{{value}}',
+    inventory: 'Inventaire invendu: {{value}}',
+    publicDemand: 'Public demand: {{value}}\f%',
+    clipsCost: 'Price per paperclip: $\f{{value}}',
+    level: 'Level: {{value}}',
+    cost: 'Cost: $\f{{value}}',
+    button: {
+      makePaperclip: 'Make paperclip',
+      buyWire: 'Wire',
+      buyAutoProducer: 'AutoClippers',
+      buyMarketing: 'Marketing',
+      decreaseClipsCost: 'Lower',
+      increaseClipsCost: 'Raise',
+    },
   },
 };
--- src/game/languages/fr.ts
+++ src/game/languages/fr.ts
@@ -3,5 +3,22 @@ import { Translation } from '../../generic/types/Translation.type';
 export const fr: Translation = {
   game: {
     title: 'Trombones',
+    manufacturing: 'Production',
+    clipsPerSecond: 'Trombones par seconde : {{value}}',
+    business: 'Entreprise',
+    fundsAvailable: 'Fonds disponibles : {{value}}\f€',
+    inventory: 'Inventaire invendu : {{value}}',
+    publicDemand: 'Demande du public : {{value}}\f%',
+    clipsCost: 'Prix par trombone : {{value}}\f€',
+    level: 'Niveau : {{value}}',
+    cost: 'Coût : {{value}}\f€',
+    button: {
+      makePaperclip: 'Produire un trombone',
+      buyWire: 'Fil de fer',
+      buyAutoProducer: 'Machines à trombones',
+      buyMarketing: 'Marketing',
+      decreaseClipsCost: 'Diminuer',
+      increaseClipsCost: 'Augmenter',
+    },
   },
 };

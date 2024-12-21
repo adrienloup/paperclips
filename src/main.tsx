da98ea915264b0f82846be3d16b3fd1d4b6437fd
Adrien Loup <adloup@gmail.com>
2024-12-21 19:27:16 +0100

    feat(1): init paperclips project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { version } from '../package.json';
 import App from './App.tsx';
 
 createRoot(document.getElementById('_paperclips_3mma_0')!).render(
   <StrictMode>
     <App />
   </StrictMode>
+);
+
+console.log(
+  `%c[paperclips]%c${version}%c@jff`,
+  'padding: 4px 3px 3px; background: #171717; font-weight: bold; color: #e6e6e6;',
+  'padding: 4px 3px 3px; background: #e6e6e6; font-weight: bold; color: #171717;',
+  'padding: 4px 3px 3px; background: #171717; font-weight: bold; color: #e6e6e6;'
 );

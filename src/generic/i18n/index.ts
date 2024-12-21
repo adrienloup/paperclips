a8d822c80456cf5bba4b7cc531e84755bb463c20
Adrien Loup <adloup@gmail.com>
2024-12-21 20:21:57 +0100

    feat(17): add i18n

--- src/generic/i18n/index.ts
+++ src/generic/i18n/index.ts
@@ -1,10 +1,10 @@
 import { initReactI18next } from 'react-i18next';
-import { useTranslation } from './useTranslation';
+import { translation } from '../utils/translation';
 import { GameTranslation } from '../../game/Game.translation';
 import i18n from 'i18next';
 
 i18n.use(initReactI18next).init({
-  resources: useTranslation(GameTranslation),
+  resources: translation(GameTranslation),
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false,

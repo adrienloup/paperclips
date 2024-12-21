3003d3ea925fc7a7558a9855b785bcd438ac05c1
Adrien Loup <adloup@gmail.com>
2024-12-21 20:12:02 +0100

    feat(1): init paperclips project

--- src/generic/components/header/Header.component.tsx
+++ src/generic/components/header/Header.component.tsx
@@ -1,10 +1,18 @@
 import { Children } from '../../types/Children.type';
+import { useLanguage } from '../../i18n/useLanguage';
 import styles from './Header.module.scss';
 
 export const HeaderComponent = ({ children }: { children: Children }) => {
+  const { language, setLanguage } = useLanguage();
+
+  const changeLanguage = () => setLanguage(language === 'fr' ? 'en' : 'fr');
+
   return (
     <header className={styles.header} role="banner">
       {children}
+      <button onClick={changeLanguage}>
+        {language === 'fr' ? 'en' : 'fr'}
+      </button>
     </header>
   );
 };

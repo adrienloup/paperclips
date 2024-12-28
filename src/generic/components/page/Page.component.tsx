43eefa20f114463608ce310d251a0a58d1f6d5ee
Adrien Loup <adloup@gmail.com>
2024-12-28 21:48:47 +0100

    feat(82): update page component

--- src/generic/components/page/Page.component.tsx
+++ src/generic/components/page/Page.component.tsx
@@ -1,7 +1,7 @@
-import { FooterComponent } from '../footer/Footer.component';
+import { Page } from './Page.type';
 import { HeaderComponent } from '../header/Header.component';
 import { MainComponent } from '../main/Main.component';
-import { Page } from './Page.type';
+import { FooterComponent } from '../footer/Footer.component';
 
 export const PageComponent = ({ children }: Page) => {
   return (

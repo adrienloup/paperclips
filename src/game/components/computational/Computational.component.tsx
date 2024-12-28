e26683fe850afabb7d62286b846a5325c9747934
Adrien Loup <adloup@gmail.com>
2024-12-28 21:46:43 +0100

    feat(71): add computational component

--- src/game/components/computational/Computational.component.tsx
+++ src/game/components/computational/Computational.component.tsx
@@ -1,30 +1,49 @@
 import { useTranslation } from 'react-i18next';
+import { useGame, useGameDispatch } from '../../useGame';
 // import { Computational } from './Computational.type';
 // import { formatNumber } from '../../../generic/utils/formatNumber';
 import { CardComponent } from '../../../generic/components/card/Card.component';
-// import { ButtonComponent } from '../../../generic/components/button/Button.component';
-// import { MetricComponent } from '../../../generic/components/metric/Metric.component';
+import { ButtonComponent } from '../../../generic/components/button/Button.component';
+import { MetricComponent } from '../../../generic/components/metric/Metric.component';
 import styles from './Computational.module.scss';
 
-export const ComputationalComponent = () => {
+function ComputationalComponent() {
   const { t } = useTranslation();
+  const game = useGame();
+  const setGame = useGameDispatch();
 
   return (
-    <CardComponent className={styles.card}>
-      <h2 className={styles.title}>{t('game.computational')}</h2>
-      <div className={styles.group}>
-        tutu
-        {/* <MetricComponent
-          value={t('game.price', {
-            value: formatNumber(fundsAvailable),
-          })}
-          label={t('game.fundsAvailable')}
-        />
-        <MetricComponent
-          value={formatNumber(unsoldInventory)}
-          label={t('game.unsoldInventory')}
-        /> */}
-      </div>
-    </CardComponent>
+    <>
+      {game.feature.trust ? (
+        <CardComponent className={styles.card}>
+          <h2 className={styles.title}>{t('game.computational')}</h2>
+          <div className={styles.group}>
+            <MetricComponent value={1} label="Trust" />
+            <MetricComponent value={1} label="Operation" />
+            <MetricComponent value={1} label="Creativity" />
+          </div>
+          <div className={styles.group}>
+            <ButtonComponent
+              className={styles.button}
+              onClick={() => console.log('ok')}
+            >
+              Processors
+            </ButtonComponent>
+            <MetricComponent value={1} label="Creativity" />
+          </div>
+          <div className={styles.group}>
+            <ButtonComponent
+              className={styles.button}
+              onClick={() => console.log('ok')}
+            >
+              Memory
+            </ButtonComponent>
+            <MetricComponent value={1} label="Creativity" />
+          </div>
+        </CardComponent>
+      ) : null}
+    </>
   );
-};
+}
+
+export default ComputationalComponent;

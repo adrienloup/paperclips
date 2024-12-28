3d96c75c01c7b1e14291b12bf24b24f9fe36aed3
Adrien Loup <adloup@gmail.com>
2024-12-28 21:48:17 +0100

    feat(80): update header component

--- src/generic/components/header/Header.component.tsx
+++ src/generic/components/header/Header.component.tsx
@@ -1,11 +1,35 @@
 import { useState } from 'react';
+import { useGameDispatch } from '../../../game/useGame';
 import { classNames } from '../../utils/classNames';
 import { ButtonComponent } from '../button/Button.component';
 import { IconComponent } from '../icon/Icon.component';
 import styles from './Header.module.scss';
 
 export const HeaderComponent = () => {
+  const setGame = useGameDispatch();
   const [opened, setOpen] = useState(false);
+
+  const restart = () => {
+    setGame({
+      paperclips: 2000, // 0
+      paperclipCost: 0.25,
+      unsoldInventory: 0,
+      fundsAvailable: 2000, // 0
+      feature: {
+        autoClippers: false,
+        marketing: false,
+        trust: false,
+      },
+      publicDemand: 75,
+      steelWire: 1000,
+      steelWireCost: 6.25,
+      steelWireRefill: 50,
+      autoClippers: 0,
+      autoClippersCost: 5.05,
+      marketing: 1,
+      marketingCost: 100,
+    });
+  };
 
   return (
     <header
@@ -13,7 +37,9 @@ export const HeaderComponent = () => {
       role="banner"
     >
       <div className={styles.inside}>
-        <div className={styles.inner}>header</div>
+        <div className={styles.inner}>
+          <button onClick={restart}>restart</button>
+        </div>
       </div>
       <ButtonComponent
         className={styles.button}

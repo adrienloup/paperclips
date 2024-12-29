ebc930c2cdbea8848a90345e035f48b3bc3f42a3
Adrien Loup <adloup@gmail.com>
2024-12-29 07:21:16 +0100

    feat(91): update computational component

--- src/game/components/computational/Computational.component.tsx
+++ src/game/components/computational/Computational.component.tsx
@@ -1,16 +1,39 @@
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useGame, useGameDispatch } from '../../useGame';
-// import { Computational } from './Computational.type';
-// import { formatNumber } from '../../../generic/utils/formatNumber';
+import { useGame /*useGameDispatch*/ } from '../../useGame';
+import { formatNumber } from '../../../generic/utils/formatNumber';
 import { CardComponent } from '../../../generic/components/card/Card.component';
 import { ButtonComponent } from '../../../generic/components/button/Button.component';
-import { MetricComponent } from '../../../generic/components/metric/Metric.component';
+import { DialComponent } from '../../../generic/components/dial/Dial.component';
 import styles from './Computational.module.scss';
 
 function ComputationalComponent() {
   const { t } = useTranslation();
   const game = useGame();
-  const setGame = useGameDispatch();
+  // const setGame = useGameDispatch();
+
+  // Production automatique des opérations en fonction du nombre de processeur
+  const produceOperationAndCreativity = () => {
+    console.log('1');
+    // const tutu = game.processors * 10;
+    // setGame({
+    //   ...game,
+    //   operations: tutu,
+    //   //   //   //   // creativity:
+    //   //   //   //   //   Math.log10(game.processors) * Math.pow(game.processors, 1.1) +
+    //   //   //   //   //   (game.processors - 1),
+    // });
+  };
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     // if (game.operations <= game.memory * 1000) {
+  //     produceOperationAndCreativity();
+  //     // }
+  //   }, 1e3);
+
+  //   return () => clearInterval(interval);
+  // }, [game]);
 
   return (
     <>
@@ -18,9 +41,12 @@ function ComputationalComponent() {
         <CardComponent className={styles.card}>
           <h2 className={styles.title}>{t('game.computational')}</h2>
           <div className={styles.group}>
-            <MetricComponent value={1} label="Trust" />
-            <MetricComponent value={1} label="Operation" />
-            <MetricComponent value={1} label="Creativity" />
+            <DialComponent value={1} label="Trust" />
+            <DialComponent
+              value={`${formatNumber(game.operations)} / ${formatNumber(game.memory * 1000)}`}
+              label="Operations"
+            />
+            <DialComponent value={game.creativity} label="Creativity" />
           </div>
           <div className={styles.group}>
             <ButtonComponent
@@ -29,7 +55,7 @@ function ComputationalComponent() {
             >
               Processors
             </ButtonComponent>
-            <MetricComponent value={1} label="Creativity" />
+            <DialComponent value={1} label="Units" />
           </div>
           <div className={styles.group}>
             <ButtonComponent
@@ -38,7 +64,7 @@ function ComputationalComponent() {
             >
               Memory
             </ButtonComponent>
-            <MetricComponent value={1} label="Creativity" />
+            <DialComponent value={1} label="Units" />
           </div>
         </CardComponent>
       ) : null}

5e57451cdcebae8e71292e63c7156cae8995c239
Adrien Loup <adloup@gmail.com>
2024-12-29 07:19:26 +0100

    feat(86): add business component

--- src/game/components/business/Business.component.tsx
+++ src/game/components/business/Business.component.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useGame, useGameDispatch } from '../../useGame';
+import { useInterval } from '../../../generic/hooks/useInterval';
 import { formatNumber } from '../../../generic/utils/formatNumber';
 import { CardComponent } from '../../../generic/components/card/Card.component';
 import { ButtonComponent } from '../../../generic/components/button/Button.component';
-import { MetricComponent } from '../../../generic/components/metric/Metric.component';
+import { DialComponent } from '../../../generic/components/dial/Dial.component';
 import styles from './Business.module.scss';
 
 function BusinessComponent() {
@@ -12,90 +12,96 @@ function BusinessComponent() {
   const game = useGame();
   const setGame = useGameDispatch();
 
-  const increasePaperclipCost = () =>
-    setGame({
-      ...game,
-      paperclipCost: Math.min(game.paperclipCost + 0.01, 1),
-    });
+  const increasePaperclipCost = () => {
+    const paperclipCost = Math.min(game.paperclipCost + 0.01, 1);
 
-  const decreasePaperclipCost = () =>
     setGame({
       ...game,
-      paperclipCost: Math.max(game.paperclipCost - 0.01, 0.01),
+      paperclipCost,
+      publicDemand: Math.max(
+        0,
+        game.marketing * 100 +
+          game.marketing -
+          paperclipCost * (game.marketing * 100 + game.marketing)
+      ),
     });
+  };
+
+  const decreasePaperclipCost = () => {
+    const paperclipCost = Math.max(game.paperclipCost - 0.01, 0.01);
+
+    setGame({
+      ...game,
+      paperclipCost,
+      publicDemand: Math.max(
+        0,
+        game.marketing * 100 +
+          game.marketing -
+          paperclipCost * (game.marketing * 100 + game.marketing)
+      ),
+    });
+  };
 
   // Acheter des niveaux de marketing
   const buyMarketing = () => {
-    if (game.fundsAvailable >= game.marketingCost && game.marketing <= 10) {
+    if (game.fundsAvailable >= game.marketingCost && game.marketing < 10) {
+      const marketing = game.marketing + 1;
+
       setGame({
         ...game,
-        marketing: game.marketing + 1,
-        marketingCost: game.marketingCost * 2,
+        marketing,
+        marketingCost: game.marketingCost * 2.5,
         fundsAvailable: game.fundsAvailable - game.marketingCost,
+        publicDemand: Math.max(
+          0,
+          marketing * 100 +
+            marketing -
+            game.paperclipCost * (marketing * 100 + marketing)
+        ),
+        steelWireRefill:
+          marketing >= 8
+            ? 200000
+            : marketing >= 6
+              ? 100000
+              : marketing >= 4
+                ? 10000
+                : marketing >= 2
+                  ? 1000
+                  : 50,
       });
     }
   };
 
   // Ventes de trombones en fonction de la demande dans la limite de l'inventaire
-  const sell = () => {
-    const sales = Math.min(
-      game.unsoldInventory,
-      game.unsoldInventory * (game.publicDemand / 100) * 2
-    );
-    setGame({
-      ...game,
-      fundsAvailable: game.fundsAvailable + sales * game.paperclipCost,
-      unsoldInventory: Math.floor(game.unsoldInventory - sales),
-    });
-  };
-
-  // Demande publique
-  useEffect(() => {
-    const bonus = game.marketing * 101;
-    setGame({
-      ...game,
-      publicDemand: Math.max(0, bonus - game.paperclipCost * bonus),
-    });
-  }, [game.marketing, game.paperclipCost]);
-
-  // Niveaux de marketing
-  useEffect(() => {
-    setGame({
-      ...game,
-      steelWireRefill:
-        game.marketing >= 5
-          ? 10000
-          : game.marketing >= 3
-            ? 1000
-            : game.steelWireRefill,
-    });
-  }, [game.marketing]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (game.unsoldInventory > 0) {
-        sell();
-      }
-    }, 1e3);
-
-    return () => clearInterval(interval);
-  }, [game]);
+  useInterval(() => {
+    if (game.unsoldInventory > 0) {
+      const sales = Math.min(
+        game.unsoldInventory,
+        game.unsoldInventory * (game.publicDemand / 100) * 2
+      );
+      setGame({
+        ...game,
+        fundsAvailable: game.fundsAvailable + sales * game.paperclipCost,
+        unsoldInventory: Math.floor(game.unsoldInventory - sales),
+      });
+    }
+  }, 1e3);
 
   return (
     <CardComponent className={styles.card}>
       <h2 className={styles.title}>{t('game.business')}</h2>
       <div className={styles.group}>
-        <MetricComponent
-          value={game.unsoldInventory}
+        <DialComponent
+          value={formatNumber(game.unsoldInventory)}
           label={t('game.unsoldInventory')}
         />
-        <MetricComponent
+        <DialComponent
           value={t('game.percent', {
             value: game.publicDemand.toFixed(0),
           })}
           label={t('game.publicDemand')}
         />
-        <MetricComponent
+        <DialComponent
           value={t('game.price', {
             value: formatNumber(game.fundsAvailable),
           })}
@@ -115,9 +121,9 @@ function BusinessComponent() {
         >
           {t('game.button.increaseClipsCost')}
         </ButtonComponent>
-        <MetricComponent
+        <DialComponent
           value={t('game.price', {
-            value: formatNumber(game.paperclipCost),
+            value: formatNumber(game.paperclipCost, 2, 2),
           })}
           label={t('game.paperclipCost')}
         />
@@ -127,11 +133,11 @@ function BusinessComponent() {
           <ButtonComponent className={styles.button} onClick={buyMarketing}>
             {t('game.button.buyMarketing')}
           </ButtonComponent>
-          <MetricComponent
+          <DialComponent
             value={game.marketing.toFixed(0)}
             label={t('game.level')}
           />
-          <MetricComponent
+          <DialComponent
             value={t('game.price', {
               value: formatNumber(game.marketingCost),
             })}

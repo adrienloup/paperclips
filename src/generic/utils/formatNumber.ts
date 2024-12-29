b3be407c569ba627f073d3761a65a5a9834a6c5d
Adrien Loup <adloup@gmail.com>
2024-12-29 07:24:21 +0100

    feat(99): update utils

--- src/generic/utils/formatNumber.ts
+++ src/generic/utils/formatNumber.ts
@@ -1,6 +1,12 @@
-export const formatNumber = (number: number) =>
-  number.toLocaleString('en-US', {
-    maximumFractionDigits: 2,
+export const formatNumber = (
+  number: number,
+  min: number = 0,
+  max: number = 1,
+  locales: string = 'en-US'
+) =>
+  number.toLocaleString(locales, {
+    minimumFractionDigits: min,
+    maximumFractionDigits: max,
     notation: 'compact',
     compactDisplay: 'short',
   });

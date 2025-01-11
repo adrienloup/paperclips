f3fc362a1e318b960d828f86258edf978e159233
Adrien Loup <adloup@gmail.com>
2025-01-11 09:18:50 +0100

    feat(105): update custom hooks

--- src/generic/hooks/useLocalStorage.ts
+++ src/generic/hooks/useLocalStorage.ts
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 
-export function useLocalStorage<V>(
-  key: string,
-  initialValue: V
-): [V, (v: V) => void] {
+export function useLocalStorage<V>(key: string, initialValue: V): [V, (v: V) => void] {
   const localValue = () => {
     try {
       return JSON.parse(localStorage.getItem(key) ?? '') as V;

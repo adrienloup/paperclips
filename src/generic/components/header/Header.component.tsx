698d493949265818101f8fcd339e4082b4793e95
Adrien Loup <adloup@gmail.com>
2025-01-11 09:17:08 +0100

    feat(101): add component

--- src/generic/components/header/Header.component.tsx
+++ src/generic/components/header/Header.component.tsx
@@ -1,25 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { classNames } from '../../utils/classNames';
 import { ButtonComponent } from '../button/Button.component';
 import { IconComponent } from '../icon/Icon.component';
 import styles from './Header.module.scss';
 
 export const HeaderComponent = () => {
-  const [opened, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onClick = () => setOpen(!open);
+
+  useEffect(() => {
+    const onKeyDown = (e: { keyCode: number }) => {
+      if (e.keyCode === 27 && open) setOpen(false);
+    };
+    document.addEventListener('keydown', onKeyDown);
+    return () => document.removeEventListener('keydown', onKeyDown);
+  }, [open]);
 
   return (
-    <header
-      className={classNames([styles.header, opened ? styles.opened : ''])}
-      role="banner"
-    >
+    <header className={classNames([styles.header, open ? styles.open : ''])} role="banner">
       <div className={styles.inside}>
         <div className={styles.inner}>header</div>
       </div>
       <ButtonComponent
         className={styles.button}
-        onClick={() => setOpen(!opened)}
+        aria-label={open ? 'Fermer les paramètres' : 'Ouvrir les paramètres'}
+        onClick={onClick}
       >
-        <IconComponent icon="tune" />
+        <IconComponent icon={open ? 'arrow_menu_close' : 'arrow_menu_open'} />
       </ButtonComponent>
     </header>
   );

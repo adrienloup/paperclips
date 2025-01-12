9790d1b3e6e065b7ca1a8660496bdfb453a46022
Adrien Loup <adloup@gmail.com>
2025-01-12 01:47:50 +0100

    feat(106): update config

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react-hooks/exhaustive-deps': 'off',
+      'no-case-declarations': 'off',
     },
   }
 );

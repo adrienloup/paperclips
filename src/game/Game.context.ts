516a5a333c60a87d9a21ddb0f6817242163455a7
Adrien Loup <adloup@gmail.com>
2025-01-12 01:48:49 +0100

    feat(108): add page

--- src/game/Game.context.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, createContext } from 'react';
-import { Game } from './Game.type';
-import { initialValue } from './Game.data';
-
-export const GameContext = createContext<Game>(initialValue);
-
-export const GameDispatchContext = createContext<Dispatch<Game>>(() => {});

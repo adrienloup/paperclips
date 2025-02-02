9c44452ac92e8fa8ebb519da569c0d8b801c9891
adrienloup <adrienloup@users.noreply.github.com>
2025-02-02 05:29:30 +0000

    deploy: 1bf28b6a9b8887532a84c6a00e6f1892e4c1246c

--- assets/Dashboard.component--nVb98sc.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-CKi6pajB.js";const s="_dashboard_1virr_1",r={dashboard:s};function d(){return a.jsx("article",{className:r.dashboard,children:"dashboard"})}export{d as default};

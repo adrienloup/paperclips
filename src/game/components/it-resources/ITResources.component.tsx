14fbcae4bb862ab8c0092be71dfdaa1436e7c66a
Adrien Loup <adloup@gmail.com>
2025-02-16 04:53:43 +0100

    feat(139): add it-resources component

--- src/game/components/it-resources/ITResources.component.tsx
+++ src/game/components/it-resources/ITResources.component.tsx
@@ -1,22 +1,22 @@
 import { useDashboard, useDashboardDispatch } from '@/src/game/components/dashboard/useDashboard';
-import { CardComponent } from '@/src/generic/common/components/cards/Card.component';
+import { CardComponent } from '@/src/generic/common/components/card/Card.component.tsx';
 import { TitleComponent } from '@/src/generic/common/components/title/Title.component';
 import { DialComponent } from '@/src/generic/common/components/dial/Dial.component';
-import { CardGroupComponent } from '@/src/generic/common/components/cards/CardGroup.component';
+import { GroupComponent } from '@/src/generic/common/components/group/Group.component.tsx';
 import { ButtonComponent } from '@/src/generic/common/components/button/Button.component';
-import styles from '@/src/generic/common/components/cards/Card.module.scss';
+import styles from '@/src/generic/common/components/card/Card.module.scss';
 
 export const ITResourcesComponent = () => {
   const setDashboard = useDashboardDispatch();
   const dashboard = useDashboard();
 
   return (
-    <CardComponent>
+    <CardComponent className={styles.cardC}>
       <TitleComponent
         className={styles.title}
         title="IT Resources"
       />
-      <CardGroupComponent>
+      <GroupComponent>
         <DialComponent
           number={dashboard.trustCost}
           notation="compact"
@@ -29,9 +29,9 @@ export const ITResourcesComponent = () => {
           label="Level"
           unit="/ 100"
         />
-      </CardGroupComponent>
-      <CardGroupComponent>
-        <CardGroupComponent direction="column">
+      </GroupComponent>
+      <GroupComponent>
+        <GroupComponent direction="column">
           <DialComponent
             number={dashboard.processors}
             notation="compact"
@@ -44,8 +44,8 @@ export const ITResourcesComponent = () => {
           >
             Augmenter
           </ButtonComponent>
-        </CardGroupComponent>
-        <CardGroupComponent direction="column">
+        </GroupComponent>
+        <GroupComponent direction="column">
           <DialComponent
             number={dashboard.memory}
             notation="compact"
@@ -58,8 +58,8 @@ export const ITResourcesComponent = () => {
           >
             Augmenter
           </ButtonComponent>
-        </CardGroupComponent>
-      </CardGroupComponent>
+        </GroupComponent>
+      </GroupComponent>
     </CardComponent>
   );
 };
